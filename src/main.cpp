8fddd838a57b49784a1c2a6ca09061a24015b923
Sébastien Roccaserra <sroccaserra@yahoo.com>
2019-12-07 22:38:33 +0100

    List layers installed on the system

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,17 @@ int main(int argc, const char * argv[]) {
     VkInstance instance;
     VkResult result;
     VkInstanceCreateInfo info = {};
+    uint32_t instance_layer_count;
+
+    result = vkEnumerateInstanceLayerProperties(&instance_layer_count, nullptr);
+    std::cout << instance_layer_count << " layers found!\n";
+    if (instance_layer_count > 0) {
+        std::unique_ptr<VkLayerProperties[]> instance_layers(new VkLayerProperties[instance_layer_count]);
+        result = vkEnumerateInstanceLayerProperties(&instance_layer_count, instance_layers.get());
+        for (int i = 0; i < instance_layer_count; ++i) {
+            std::cout << instance_layers[i].layerName << "\n";
+        }
+    }
 
     result = vkCreateInstance(&info, NULL, &instance);
     std::cout << "vkCreateInstance result: " << result  << "\n";
